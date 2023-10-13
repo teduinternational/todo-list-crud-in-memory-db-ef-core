c8745d8263953105b9eed5ba98499aa4c769453a
Bach Ngoc Toan <ngoctoan@live.com>
2023-10-14 06:21:55 +0700

    Initial

---
+++ TodoList.Api/Data/Todo.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TodoList.Api.Data
+{
+    [Table("Todos")]
+    public class Todo
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public required string Title { get; set; }
+
+        [Required]        
+        public Priority Priority { get; set; }
+        public DateTime? Deadline { get; set; }
+        public Status Status { get; set; } = Status.Todo;
+
+        [MaxLength(100)]
+        public string? Assignee { get; set; }
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+        [Required]
+        public Guid AnonymousId { get; set; }
+    }
+    public enum Status
+    {
+        Todo,
+        InProgress,
+        Done,
+        Canceled
+    }
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High,
+        Urgent
+    }
+}
---
+++ TodoList.Api/Data/TodoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoList.Api.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
